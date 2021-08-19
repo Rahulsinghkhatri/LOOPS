ab128a7038d3d714b6dc5e47ba75e332e42cc0a7
RAHUL SINGH KHATRI <rahulsinghkhatri2002@gmail.com>
2021-08-19 23:31:34 +0530

    Add files via upload

---
+++ p41.cpp
@@ -0,0 +1,15 @@
+//TABLE OF ONE USING WHILE LOOP
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i=1;
+    while(i<11)
+    {
+       cout<<"1 X "<<i<<" = "<<i<<endl;
+       i++;
+    }
+
+    return 0;
+}
